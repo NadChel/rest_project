13b12b06050dd11caa56f86fada5d564b5977ded
Sergey <nad.chel@mail.ru>
2023-03-20 18:56:58 +0300

    Рабочая версия

--- src/main/java/rest_app/dao/EmployeeDaoImpl.java
+++ src/main/java/rest_app/dao/EmployeeDaoImpl.java
@@ -1,35 +1,33 @@
 package rest_app.dao;
 
-import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceContext;
+import org.hibernate.Session;
 import org.springframework.stereotype.Repository;
 import rest_app.models.Employee;
 
 import java.util.List;
 @Repository
 public class EmployeeDaoImpl implements EmployeeDao {
-    private final EntityManagerFactory entityManagerFactory;
-
-    public EmployeeDaoImpl(EntityManagerFactory entityManagerFactory) {
-        this.entityManagerFactory = entityManagerFactory;
-    }
+    @PersistenceContext
+    private Session session;
 
     @Override
     public List<Employee> getAllEmployees() {
-        return entityManagerFactory.createEntityManager().createQuery("from Employee", Employee.class).getResultList();
+        return session.createQuery("from Employee", Employee.class).getResultList();
     }
 
     @Override
     public void addEmployee(Employee employee) {
-        entityManagerFactory.createEntityManager().merge(employee);
+        session.merge(employee);
     }
 
     @Override
     public Employee getEmployeeById(long id) {
-        return entityManagerFactory.createEntityManager().getReference(Employee.class, id);
+        return session.get(Employee.class, id);
     }
 
     @Override
     public void deleteEmployeeById(long id) {
-        entityManagerFactory.createEntityManager().remove(this.getEmployeeById(id));
+        session.remove(this.getEmployeeById(id));
     }
 }
